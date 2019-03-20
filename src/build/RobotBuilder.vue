892894ac4caca0693589650b86da301e4418b6a2
Eric Nshimiye <enshimiye@dialoginsight.com>
2019-03-20 07:55:49 -0400

    Preventing navigation away from pages with navigation guards

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -74,9 +74,20 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   components: { PartSelector, CollapsibleSection },
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-restricted-globals: 0 */
+      /* eslint no-alert: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave ?');
+      next(response);
+    }
+  },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -99,6 +110,7 @@ export default {
        + robot.rightArm.cost + robot.base.cost;
 
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
