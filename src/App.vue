b814187eb6034a522cec8a848b7bde643c6d162b
ericnshimiye <ericnsh@gmail.com>
2019-03-20 05:45:40 -0400

    Using named views

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,55 @@
         <ul>
           <li class="nav-item">
             <router-link class="nav-link" :to="{name: 'Home'}" exact>
-              <img class="logo" src="./assets/build-a-bot-logo.png"/>
+              <img class="logo" src="./assets/build-a-bot-logo.png">
               Build-a-Bot
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Build'}" exact>
-              Build
-            </router-link>
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>Build</router-link>
           </li>
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view/>
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app',
 };
 </script>
 
 <style>
-
-  body{
-    background: linear-gradient(to bottom, #555, #999);
-    background-attachment: fixed;
-  }
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 </style>
 
 <style scoped>
-
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 
-main{
-  margin: 0 auto;
+main {
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -71,11 +70,22 @@ ul {
   vertical-align: middle;
   height: 30px;
 }
-.nav-link{
+.nav-link {
   text-decoration: none;
-  color:inherited;
+  color: inherited;
 }
-.router-link-active{
+.router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
